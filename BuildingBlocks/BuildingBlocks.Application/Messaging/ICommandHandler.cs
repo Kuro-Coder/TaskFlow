773041f97e8a88e2700ce7e52d5de7ecffc93e5e
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-02 23:19:09 +0330

    feat: add all needed BB.App class

---
+++ BuildingBlocks/BuildingBlocks.Application/Messaging/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BuildingBlocks.Application.Messaging;
+
+public interface ICommandHandler<TCommand, TResult>
+{
+    Task<TResult> Handle(
+        TCommand command,
+        CancellationToken cancellationToken);
+}
