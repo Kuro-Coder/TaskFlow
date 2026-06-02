3ed673f1fb93f289534d38c1a898e88869e79ea9
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-02 22:45:20 +0330

    feat: step 1 in BB.Domain

---
+++ BuildingBlocks/BuildingBlocks.Domain/Abstractions/AggregateRoot.cs
@@ -0,0 +1,20 @@
+﻿
+namespace BuildingBlocks.Domain.Abstractions;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents
+        => _domainEvents.AsReadOnly();
+
+    protected void Raise(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
