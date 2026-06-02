6613081e8f91a0ac998952d2e6629f0fee51cf90
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-02 23:35:20 +0330

    feat: add project entity repo and more changes

---
+++ Modules/Projects/Projects.Domain/Errors/ProjectErrors.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Projects.Domain.Errors;
+
+public static class ProjectErrors
+{
+    //public static readonly Error NameIsRequired =
+    //    new(
+    //        "Projects.NameIsRequired",
+    //        "Project name is required.");
+}
