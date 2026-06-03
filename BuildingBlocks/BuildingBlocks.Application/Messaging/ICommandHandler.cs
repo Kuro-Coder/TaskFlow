a2629fee5c4baa627b922162d0bbdc17d3983d86
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-03 20:50:18 +0330

    feat: new changes for command

--- BuildingBlocks/BuildingBlocks.Application/Messaging/ICommandHandler.cs
+++ BuildingBlocks/BuildingBlocks.Application/Messaging/ICommandHandler.cs
@@ -1,9 +1,19 @@
 ﻿
+using BuildingBlocks.Application.Results;
+
 namespace BuildingBlocks.Application.Messaging;
 
 public interface ICommandHandler<TCommand, TResult>
+    where TCommand : ICommand<TResult>
 {
-    //Task<TResult> Handle(
-    //    TCommand command,
-    //    CancellationToken cancellationToken);
+    Task<Result<TResult>> Handle(
+        TCommand command,
+        CancellationToken cancellationToken);
+}
+public interface ICommandHandler<TCommand>
+    where TCommand : ICommand
+{
+    Task<Result> Handle(
+        TCommand command,
+        CancellationToken cancellationToken);
 }
