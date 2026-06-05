3058e9f6447bdfdd623bca35cb2e464b371ddf84
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-05 22:58:58 +0330

    feat: add paged result

---
+++ BuildingBlocks/BuildingBlocks.Application/Common/PaginationRequest.cs
@@ -0,0 +1,6 @@
+﻿
+namespace BuildingBlocks.Application.Common;
+
+public sealed record PaginationRequest(
+    int Page = 1,
+    int PageSize = 20);
---
+++ BuildingBlocks/BuildingBlocks.Domain/Shared/PagedResult.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BuildingBlocks.Domain.Shared;
+
+public sealed record PagedResult<T>(
+    IReadOnlyCollection<T> Items,
+    int Page,
+    int PageSize,
+    int TotalCount)
+{
+    public int TotalPages =>
+        (int)Math.Ceiling(
+            TotalCount / (double)PageSize);
+}
