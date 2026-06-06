168b36afadf634d9d2281b9aee7961558e5d0c9d
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-06 22:25:57 +0330

    feat: add ICurrentUser interface for new chanegs

---
+++ BuildingBlocks/BuildingBlocks.Application/Abstractions/ICurrentUser.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BuildingBlocks.Application.Abstractions;
+
+public interface ICurrentUser
+{
+    Guid? UserId { get; }
+
+    string? UserName { get; }
+
+    bool IsAuthenticated { get; }
+}
