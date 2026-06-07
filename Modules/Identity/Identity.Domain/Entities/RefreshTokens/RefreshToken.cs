adfb97d2d48b9b20c73bf1a18bfbfa4a2d499839
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-07 22:31:33 +0330

    feat: add refresh token entity

---
+++ Modules/Identity/Identity.Domain/Entities/RefreshTokens/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Identity.Domain.Entities.RefreshTokens;
+
+public sealed class RefreshToken
+{
+    public Guid Id { get; private set; }
+
+    public string Token { get; private set; }
+
+    public DateTime ExpiresOnUtc { get; private set; }
+
+    public bool IsRevoked { get; private set; }
+
+    private RefreshToken()
+    {
+    }
+}
