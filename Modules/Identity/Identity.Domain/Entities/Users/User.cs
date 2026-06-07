c975cb27856ae796e85ef08fdb3d6a056bd7ed70
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-07 22:31:17 +0330

    feat: add user entity

---
+++ Modules/Identity/Identity.Domain/Entities/Users/User.cs
@@ -0,0 +1,55 @@
+﻿using BuildingBlocks.Domain.Abstractions;
+
+namespace Identity.Domain.Entities.Users;
+
+public sealed class User
+    : AuditableAggregateRoot<Guid>
+{
+    private User()
+    {
+    }
+
+    private User(
+        Guid id,
+        string firstName,
+        string lastName,
+        string email,
+        string passwordHash)
+        : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PasswordHash = passwordHash;
+        IsActive = true;
+    }
+
+    public string FirstName { get; private set; }
+
+    public string LastName { get; private set; }
+
+    public string Email { get; private set; }
+
+    public string PasswordHash { get; private set; }
+
+    public bool IsActive { get; private set; }
+
+    public static User Create(
+        string firstName,
+        string lastName,
+        string email,
+        string passwordHash)
+    {
+        return new User(
+            Guid.NewGuid(),
+            firstName,
+            lastName,
+            email,
+            passwordHash);
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
+    }
+}
