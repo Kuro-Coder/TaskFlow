daac324f3049072572b3688028ae205fe2edf7ca
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-07 23:04:34 +0330

    feat: add jwt and hash pass intefaces

---
+++ Modules/Identity/Identity.Application/Abstractions/IJwtProvider.cs
@@ -0,0 +1,10 @@
+﻿
+using Identity.Domain.Entities.Users;
+
+namespace Identity.Application.Abstractions;
+
+public interface IJwtProvider
+{
+    string Generate(
+        User user);
+}
---
+++ Modules/Identity/Identity.Application/Abstractions/IPasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Identity.Application.Abstractions;
+
+public interface IPasswordHasher
+{
+    string Hash(
+        string password);
+
+    bool Verify(
+        string password,
+        string passwordHash);
+}
