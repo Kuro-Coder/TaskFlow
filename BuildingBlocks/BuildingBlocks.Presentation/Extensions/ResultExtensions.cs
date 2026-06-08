2a53cbda3bc1f9ef9693e27d56129138c74a91ad
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-08 22:18:23 +0330

    feat: add BB.Presntation layer

---
+++ BuildingBlocks/BuildingBlocks.Presentation/Extensions/ResultExtensions.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.Application.Results;
+using Microsoft.AspNetCore.Http;
+
+namespace BuildingBlocks.Presentation.Extensions;
+
+public static class ResultExtensions
+{
+    public static IResult ToProblemResult(
+        this Result result)
+    {
+        if (result.IsSuccess)
+            throw new InvalidOperationException();
+
+        return Results.Problem(
+            title: result.Error!.Code,
+            detail: result.Error.Message,
+            statusCode: StatusCodes.Status400BadRequest);
+    }
+}
