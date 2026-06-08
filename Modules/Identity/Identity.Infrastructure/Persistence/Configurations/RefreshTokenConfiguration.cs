d208bab8b9f91d42324d7322918fa735ea6e2043
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-08 22:59:15 +0330

    feat: modify configurations

---
+++ Modules/Identity/Identity.Infrastructure/Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Identity.Domain.Entities.RefreshTokens;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Identity.Infrastructure.Persistence.Configurations;
+
+public sealed class RefreshTokenConfiguration
+    : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(
+        EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Token)
+            .IsRequired();
+
+        builder.Property(x => x.ExpiresOnUtc)
+            .IsRequired();
+
+        builder.Property(x => x.IsRevoked)
+            .IsRequired();
+
+    }
+}
---
+++ Modules/Identity/Identity.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Identity.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Identity.Infrastructure.Persistence.Configurations;
+
+public sealed class UserConfiguration
+    : IEntityTypeConfiguration<User>
+{
+    public void Configure(
+        EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.FirstName)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.LastName)
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Email)
+            .HasMaxLength(200);
+
+        builder.Property(x => x.PasswordHash)
+            .HasMaxLength(500);
+
+        builder.Property(x => x.IsActive)
+            .IsRequired();
+    }
+}
