10afeaf97834cc2d73e959055742e78166bdb1de
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-10 22:13:02 +0330

    add AccessTokenResponse model

---
+++ Modules/Identity/Identity.Application/Users/AccessTokenResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Users;
+
+public sealed record AccessTokenResponse(
+    string AccessToken,
+    DateTime ExpiresAtUtc);
