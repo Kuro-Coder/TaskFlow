5dcf5cc0d5a3976acd256cd8f2a96cc5e591717c
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-10 22:13:55 +0330

    feat: add login endpoint

---
+++ Modules/Identity/Identity.Presentation/Features/Login/LoginEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BuildingBlocks.Application.Messaging;
+using BuildingBlocks.Presentation.Extensions;
+using Identity.Application.Users.Commands.Login;
+
+namespace Identity.Presentation.Features.Login;
+
+[ApiController]
+[Route("api/identity/Login")]
+[Tags("Identity")]
+public sealed class LoginEndpoint : ControllerBase
+{
+    private readonly ICommandDispatcher _dispatcher;
+
+    public LoginEndpoint(
+        ICommandDispatcher dispatcher)
+    {
+        _dispatcher = dispatcher;
+    }
+
+    [HttpPost]
+    public async Task<IResult> Handle(
+        LoginRequest request,
+        CancellationToken cancellationToken)
+    {
+        var result = await _dispatcher.Dispatch(new LoginUserCommand(
+            request.Email,
+            request.Password),
+            cancellationToken);
+        if (result.IsFailure)
+            return result.ToProblemResult();
+
+        return Results.Ok(result.Value);
+    }
+}
---
+++ Modules/Identity/Identity.Presentation/Features/Login/LoginRequest.cs
@@ -0,0 +1,6 @@
+﻿
+namespace Identity.Presentation.Features.Login;
+
+public sealed record LoginRequest(
+    string Email,
+    string Password);
