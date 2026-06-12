d9260cbaf0632c811c9f0a036ea34163a226aefb
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-12 22:36:48 +0330

    feat: register jwt and authorize token on swagger

--- Host/TaskFlow.Api/Program.cs
+++ Host/TaskFlow.Api/Program.cs
@@ -2,8 +2,12 @@ using BuildingBlocks.Application.Abstractions;
 using BuildingBlocks.Infrastructure;
 using Identity.Infrastructure;
 using Identity.Presentation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Projects.Infrastructure;
 using Projects.Presentation;
+using System.Text;
 using TaskFlow.Api.Authentication;
 using TaskFlow.Api.Extensions;
 
@@ -24,12 +28,79 @@ builder.Services.AddScoped<ICurrentUser, CurrentUser>();
 builder.Services.AddProjectsInfrastructure(
     builder.Configuration.GetConnectionString("DefaultConnection")!);
 
-builder.Services.AddIdentitiesInfrastructure(
-    builder.Configuration.GetConnectionString("DefaultConnection")!);
+builder.Services.AddIdentitiesInfrastructure(builder.Configuration!);
 
 builder.Services.AddProjectsPresentation();
 
 builder.Services.AddIdentityPresentation();
+var jwtSection =
+    builder.Configuration.GetSection("Jwt");
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "TaskFlow API",
+            Version = "v1"
+        });
+
+    options.AddSecurityDefinition(
+        "Bearer",
+        new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme."
+        });
+
+    options.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+});
+
+var secretKey = jwtSection["SecretKey"]!;
+builder.Services
+    .AddAuthentication(
+        JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer =
+                    jwtSection["Issuer"],
+
+                ValidAudience =
+                    jwtSection["Audience"],
+
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(
+                            secretKey))
+            };
+    });
 
 var app = builder.Build();
 
@@ -38,6 +109,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
 app.UseExceptionHandling();
 
 app.UseHttpsRedirection();
