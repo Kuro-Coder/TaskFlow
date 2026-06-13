fe8b4e82fbadd3f12dfff693f33f78c6e317babf
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-13 22:52:16 +0330

    feat: add CurentUser endpoint

---
+++ Modules/Identity/Identity.Presentation/Features/CurentUser/CurentUserEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.Application.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Presentation.Features.CurentUser;
+
+[ApiController]
+[Route("api/identity/CurentUser")]
+[Tags("Identity")]
+public sealed class CurentUserEndpoint : ControllerBase
+{
+    public CurentUserEndpoint()
+    {
+    }
+
+    [HttpGet("me")]
+    [Authorize]
+    public IResult Me(
+        [FromServices] ICurrentUser currentUser)
+    {
+        return Results.Ok(
+            new
+            {
+                currentUser.UserId
+            });
+    }
+}
