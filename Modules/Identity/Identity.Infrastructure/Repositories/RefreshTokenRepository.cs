0dba5784790ef5f35a1283cf5b18c93eae104335
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-14 21:54:16 +0330

    feat: add refresh token repository

---
+++ Modules/Identity/Identity.Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,37 @@
+﻿using Identity.Domain.Entities.RefreshTokens;
+using Identity.Domain.Repositories;
+using Identity.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Infrastructure.Repositories;
+
+public sealed class RefreshTokenRepository
+    : IRefreshTokenRepository
+{
+    private readonly IdentityDbContext _context;
+
+    public RefreshTokenRepository(
+        IdentityDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(
+        RefreshToken refreshToken,
+        CancellationToken cancellationToken)
+    {
+        await _context.RefreshTokens.AddAsync(
+            refreshToken,
+            cancellationToken);
+    }
+
+    public async Task<RefreshToken?> GetByTokenAsync(
+        string token,
+        CancellationToken cancellationToken)
+    {
+        return await _context.RefreshTokens
+            .FirstOrDefaultAsync(
+                x => x.Token == token,
+                cancellationToken);
+    }
+}
