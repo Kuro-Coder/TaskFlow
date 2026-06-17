c136d9564a8157a4e9e724e3b9f809929fd5453a
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-17 22:51:56 +0330

    feat: add task entity

---
+++ Modules/Tasks/Tasks.Domain/Entities/Task.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.Domain.Abstractions;
+using Tasks.Domain.Entities.Enums;
+
+namespace Tasks.Domain.Entities;
+
+public sealed class TaskItem : AuditableAggregateRoot<Guid>, ISoftDelete
+{
+    public Guid ProjectId { get; private set; }
+
+    public string Title { get; private set; }
+
+    public string? Description { get; private set; }
+
+    public Enums.TaskStatus Status { get; private set; }
+
+    public TaskPriority Priority { get; private set; }
+
+    public DateTime? DueDateUtc { get; private set; }
+
+    public bool IsDeleted { get; private set; }
+
+    public DateTime? DeletedOnUtc { get; private set; }
+}
