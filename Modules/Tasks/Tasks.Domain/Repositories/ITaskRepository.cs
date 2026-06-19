f1a52c7968ee61698545a3ec194f266fdeb71391
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-19 22:46:37 +0330

    feat: add repository interface

---
+++ Modules/Tasks/Tasks.Domain/Repositories/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using Tasks.Domain.Entities;
+
+namespace Tasks.Domain.Repositories;
+
+public interface ITaskRepository
+{
+    Task AddAsync(
+        TaskItem task,
+        CancellationToken cancellationToken);
+
+    Task<TaskItem?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken);
+}
