7f7e2f5fc4a7988cccab32e02a53ff7e19007741
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2026-06-19 22:46:54 +0330

    feat: add taskItem configuration

---
+++ Modules/Tasks/Tasks.Infrastructure/Persistence/Configurations/TaskItemConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tasks.Domain.Entities;
+
+namespace Tasks.Infrastructure.Persistence.Configurations;
+
+public sealed class TaskItemConfiguration
+    : IEntityTypeConfiguration<TaskItem>
+{
+    public void Configure(
+        EntityTypeBuilder<TaskItem> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(2000);
+
+        builder.Property(x => x.Status)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.Property(x => x.Priority)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.Ignore(x => x.DomainEvents);
+    }
+}
